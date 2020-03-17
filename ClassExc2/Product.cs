be92787878ec14a91fac3e16e5b9ec819f168801
LIDAUHaipengLiu <55245401+LIDAUHaipengLiu@users.noreply.github.com>
2020-03-17 03:47:38 -0700

    Add files via upload

---
+++ ClassExc2/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassExc2
+{
+    public class Product
+    {
+        public String _name;
+        public double _price;
+
+        public void Buy(int quantity, string type)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void JinHuo(int quantity, string type)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
